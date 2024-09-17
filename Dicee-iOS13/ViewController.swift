e3f921a2e82ae25c664227ab7a62c882d8ebfa68
its-ankur <ankuratul2007@gmail.com>
2024-09-17 12:49:34 +0530

    code improved

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -21,38 +21,8 @@ class ViewController: UIViewController {
 
     
     @IBAction func btnClicked(_ sender: Any) {
-        let dice1=Int.random(in: 0...6)
-        let dice2=Int.random(in: 0...6)
-        if dice1==2{
-            diceImageView1.image=UIImage(named: "DiceTwo")
-        }
-        if dice1==3{
-            diceImageView1.image=UIImage(named: "DiceThree")
-        }
-        if dice1==4{
-            diceImageView1.image=UIImage(named: "DiceFour")
-        }
-        if dice1==5{
-            diceImageView1.image=UIImage(named: "DiceFive")
-        }
-        if dice1==6{
-            diceImageView1.image=UIImage(named: "DiceSix")
-        }
-        if dice2==1{
-            diceImageView2.image=UIImage(named: "DiceOne")
-        }
-        if dice2==3{
-            diceImageView2.image=UIImage(named: "DiceThree")
-        }
-        if dice2==4{
-            diceImageView2.image=UIImage(named: "DiceFour")
-        }
-        if dice2==5{
-            diceImageView2.image=UIImage(named: "DiceFive")
-        }
-        if dice2==6{
-            diceImageView2.image=UIImage(named: "DiceSix")
-        }
+        diceImageView1.image=[UIImage(named: "DiceOne"),UIImage(named: "DiceTwo"),UIImage(named: "DiceThree"),UIImage(named: "DiceFour"),UIImage(named: "DiceFive"),UIImage(named: "DiceSix")][Int.random(in: 0...5)]
+        diceImageView2.image=[UIImage(named: "DiceOne"),UIImage(named: "DiceTwo"),UIImage(named: "DiceThree"),UIImage(named: "DiceFour"),UIImage(named: "DiceFive"),UIImage(named: "DiceSix")][Int.random(in: 0...5)]
     }
     
 }
